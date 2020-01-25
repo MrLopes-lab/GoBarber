5518bfbebc23100ab6f1ecc04d423199b0a1f1eb
MrDev <lopesrodrigues05@gmail.com>
2020-01-24 21:50:49 -0300

    Cancel de agend. & Marcar agend. como lida

--- src/routes.js
+++ src/routes.js
@@ -26,10 +26,12 @@ routes.get('/providers', ProviderController.index); // Mostra todos os prestador
 
 routes.get('/appointments', AppointmentController.index); // Lista todos os agendamentos
 routes.post('/appointments', AppointmentController.store); // Agendamento de serviço
+routes.delete('/appointments/:id', AppointmentController.delete); // Cancelar um agendamento
 
 routes.get('/schedule', SchedulesController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store); // Upload foto user
 
